8c92bbcccf9692d7d39a7f647304fd69f4974d2b
Niko <nikolaus.kapfer-rupp@stud.fh-campuswien.ac.at>
2023-03-12 22:12:19 +0100

    added surefire pluggin in pom;

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -67,7 +67,6 @@ public class HomeController implements Initializable {
     }
 
     public void initializeState() {
-
         observableMovies.clear();
         observableMovies.addAll(allMovies);
 
